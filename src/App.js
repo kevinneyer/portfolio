3eb2a1c7afceba6b0a50ca11c525d64d2d0e7a4e
kevinneyer <kevinneyer@gmail.com>
2020-08-26 17:46:17 -0400

    added image links:

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css'
 import About from './About.js'
 import Projects from './Projects.js'
 import Github from './images/Github.png'
+import LinkedIn from './images/LinkedIn.png'
+import Medium from './images/Medium.png'
 
 function App() {
   return (
@@ -10,7 +12,11 @@ function App() {
       <h1>Welcome! I am Kevin Neyer</h1>
       <About />
       <Projects />
-      <a href='https://github.com/kevinneyer' target="_blank"><img src={Github} alt='Github logo' /></a>
+        <div className='links'>
+          <a className='link' href='https://github.com/kevinneyer' target="_blank"><img width='37px' src={Github} alt='Github logo' /></a>
+          <a className='link' href='https://www.linkedin.com/in/kevin-neyer-02331665/' target="_blank"><img width='40.5px' src={LinkedIn} alt='LinkedIn logo' /></a>
+          <a className='link' href='https://medium.com/@kevinneyer' target="_blank"><img width='34.5px' src={Medium} alt='Medium logo' /></a>
+        </div>
     </div>
   )
 }
