3eff007b97cca88330abdd371e8777de06dc8ff9
kevinneyer <kevinneyer@gmail.com>
2020-08-28 16:53:45 -0400

    formatted projects into cards

--- src/About.js
+++ src/About.js
@@ -4,7 +4,7 @@ const About = () =>{
   return(
     <div>
         <h1>About</h1>
-        <div class="container-sm">
+        <div className="about" class="container-sm">
         <p class="lead">
           I am a full stack web developer experienced in Ruby and JavaScript based programming. With a background in music and operations, I bring creativity, attention to detail, and collaboration to every project.
           I enjoy problem solving, thinking creatively, and working with others. I look forward to working with you!
--- src/Projects.js
+++ src/Projects.js
@@ -1,18 +1,48 @@
 import React from 'react'
+import bierShot from './images/bierShot.png'
 
 const Projects = () =>{
   return(
     <>
       <h1>Projects</h1>
-      <div class="card" style={{width: '18em'}}>
-        <img src="..." class="card-img-top" alt="..."/>
-        <div class="card-body">
-          <h5 class="card-title">Biergarten</h5>
-          <p class="card-text">Social Media Beer Passport.</p>
-          <a href="https://www.youtube.com/watch?v=gOuxuEROFxU" class="btn btn-primary">Check out the demo!</a>
-        </div>
-    </div>
+      <div className='projectCards'>
+        <div class="card" style={{width: '20em'}}>
+          <img src={bierShot} class="card-img-top" alt="..."/>
+          <div class="card-body">
+            <h5 class="card-title">Biergarten</h5>
+            <p class="card-text">Social Media Beer Passport.</p>
+            <div class="btn-group" role="group" aria-label="Basic example">
+              <a href="https://www.youtube.com/watch?v=gOuxuEROFxU" class="btn btn-primary">Check out the demo!</a>
+              <a href="https://github.com/kevinneyer/biergarten-front-end/tree/master/biergarten-client" class="btn btn-secondary">See the code!</a>
+            </div>
+            </div>
+          </div>
 
+        <div  class="card" style={{width: '20em'}}>
+          <img src={null} class="card-img-top" alt="The Divebar"/>
+          <div class="card-body">
+            <h5 class="card-title">The Divebar</h5>
+            <p class="card-text">Create or Review a 3 Ingredient Cocktail</p>
+            <div class="btn-group" role="group" aria-label="Basic example">
+              <a href="" class="btn btn-primary">Check out the demo!</a>
+              <a href="" class="btn btn-secondary">See the code!</a>
+            </div>
+          </div>
+        </div>
+
+        <div  class="card" style={{width: '20em'}}>
+          <img src={null} class="card-img-top" alt="Purrever Home"/>
+          <div class="card-body">
+            <h5 class="card-title">Purrever Home</h5>
+            <p class="card-text">Check out what cats are up for adoption!</p>
+            <div class="btn-group" role="group" aria-label="Basic example">
+              <a href="" class="btn btn-primary">Check out the demo!</a>
+              <a href="" class="btn btn-secondary">See the code!</a>
+            </div>
+          </div>
+        </div>
+
+    </div>
     </>
   )
 }
